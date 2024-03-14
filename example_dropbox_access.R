23a6eb3365c32f3fbe4df3b9fa5f170f04bc1061
Haututu <taylor.winter@vuw.ac.nz>
2024-03-14 21:17:10 +1300

    Changing from OneDrive to DropBox

---
+++ example_dropbox_access.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(rdrop2)
+
+# Authenticate with dropbox
+drop_auth()
+
+# View files filtered by stem (easier to save to object then access path names)
+drop_dir("winter data/covid19 lockdown surveys")
+
+# Load a single file
+my_df <- drop_read_csv("winter data/rwa mturk experiment 2022/condition_codes.csv")
+
+
+# Get multiple files
+files <- rdrop2::drop_dir("winter data/covid19 lockdown surveys/momentary_level_one")$path_display
+
+# Pull each file from dropbox and bind it into a single df
+raw_data <- data.frame()
+
+for (file in 1:length(files)) {
+  print(files[file])
+  
+  raw_data <- bind_rows(
+    raw_data,
+    drop_read_csv(files[file])
+  )
+}
