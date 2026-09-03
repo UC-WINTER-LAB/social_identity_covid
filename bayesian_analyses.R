5789487942ca5bccd70c1a80c9b5d452be59659b
HunterOfSnacks <hlu53@uclive.ac.nz>
2026-09-03 14:21:28 +1200

    finalising first model for study 1

--- bayesian_analyses.R
+++ bayesian_analyses.R
@@ -1,23 +1,23 @@
-library(brms)
-library(cmdstanr)
+source("cleaning.R")
 
+#Cleaning code------------------------------------------------------------------
 analysis_df <- analysis_df %>%
   janitor::clean_names()
 
 colnames(analysis_df)
 
-priors <- c(
+#Model 1 Pol_orint -> ingroup---------------------------------------------------
+priors_1 <- c(
   prior(normal(0, 0.5), class = "b") 
 )
 
 m1 <- brm(
-  ingroup ~ political_beliefs + age + sex,
+  ingroup ~ political_party + age + sex,
   data = analysis_df,
   family = gaussian,
-  prior = priors,
+  prior = priors_1,
   chains = 4, cores = 4,
-  backend = 'cmdstan'
 )
 
-
 m1
+bayestestR::rope(m1)
